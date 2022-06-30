6303e9ebae05a5ec5dbb988e28ee1849a26b59b0
HaxxooR <92272766+HaxxooR@users.noreply.github.com>
2022-06-30 23:57:45 +0200

    Image adding

--- web-app/src/components/Gallery.vue
+++ web-app/src/components/Gallery.vue
@@ -2,23 +2,56 @@
   <v-container class="my-15">
     <v-row justify="center">
         <v-col lg="12" class="text-center ">
-            <v-subheader class="text-h4 justify-center d-none d-sm-flex ">Gallery</v-subheader>
-            <p class=" justify-center d-none d-sm-flex">Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum</p>
-            <v-subheader class="text-h4 justify-center d-flex d-sm-none">Gallery</v-subheader>
-            <p class="text-center d-flex d-sm-none">Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum</p>
+            <v-subheader class="text-h4 justify-center d-none d-sm-flex ">Galeria</v-subheader>
+            <p class=" justify-center d-none d-sm-flex">Trochę piekna z pozostałych częsci Naszego pięknego kraju!</p>
+            <v-subheader class="text-h4 justify-center d-flex d-sm-none">Galeria</v-subheader>
+            <p class="text-center d-flex d-sm-none">Trochę piekna z pozostałych częsci Naszego pięknego kraju!</p>
         </v-col>     
         
     <v-col
-      v-for="n in 9"
-      :key="n"
       class="d-none d-sm-flex "
-      cols="3"
+      cols="9"
     >
       <v-img
-        :src="`https://picsum.photos/500/300?image=${n * 5 + 10}`"
-        :lazy-src="`https://picsum.photos/10/6?image=${n * 5 + 10}`"
+        :src="'https://i.wpimg.pl/1200x/d.wpimg.pl/576964982-998273013/gory-aktywnie-weekend.jpg'"
         aspect-ratio="1"
-        class="grey lighten-2"
+        class="grey lighten-2 ml-10"
+      >
+        <template v-slot:placeholder>
+          <v-row
+            class="fill-height ma-0"
+            align="center"
+            justify="center"
+          >
+            <v-progress-circular
+              indeterminate
+              color="grey lighten-5"
+            ></v-progress-circular>
+          </v-row>
+        </template>
+      </v-img>
+       <v-img
+        :src="'https://doba.pl/media/powiaty/wroclaw/articles/images/22342/doba_pl_68666-0d19c68577d38e93fadbf0996f6d1004_1280x720.jpg'"
+        aspect-ratio="1"
+        class="grey lighten-2 ml-10"
+      >
+        <template v-slot:placeholder>
+          <v-row
+            class="fill-height ma-0"
+            align="center"
+            justify="center"
+          >
+            <v-progress-circular
+              indeterminate
+              color="grey lighten-5"
+            ></v-progress-circular>
+          </v-row>
+        </template>
+      </v-img>
+       <v-img
+        :src="'https://polskazachwyca.pl/wp-content/uploads/2017/11/moszna-shutterstock_45201820-e1511825136853.jpg'"
+        aspect-ratio="1"
+        class="grey lighten-2 ml-10"
       >
         <template v-slot:placeholder>
           <v-row
@@ -35,15 +68,52 @@
       </v-img>
     </v-col>
 
+
+
     <v-col
-      v-for="n in 9"
-      :key="n"
       class="d-flex d-sm-none"
-      cols="8"
+      cols="12"
     >
       <v-img
-        :src="`https://picsum.photos/500/300?image=${n * 5 + 10}`"
-        :lazy-src="`https://picsum.photos/10/6?image=${n * 5 + 10}`"
+        :src="'https://i.wpimg.pl/1200x/d.wpimg.pl/576964982-998273013/gory-aktywnie-weekend.jpg'"
+        aspect-ratio="1"
+        class="grey lighten-2"
+      >
+        <template v-slot:placeholder>
+          <v-row
+            class="fill-height ma-0"
+            align="center"
+            justify="center"
+          >
+            <v-progress-circular
+              indeterminate
+              color="grey lighten-5"
+            ></v-progress-circular>
+          </v-row>
+        </template>
+      </v-img>
+
+      <v-img
+        :src="'https://doba.pl/media/powiaty/wroclaw/articles/images/22342/doba_pl_68666-0d19c68577d38e93fadbf0996f6d1004_1280x720.jpg'"
+        aspect-ratio="1"
+        class="grey lighten-2"
+      >
+        <template v-slot:placeholder>
+          <v-row
+            class="fill-height ma-0"
+            align="center"
+            justify="center"
+          >
+            <v-progress-circular
+              indeterminate
+              color="grey lighten-5"
+            ></v-progress-circular>
+          </v-row>
+        </template>
+      </v-img>
+
+      <v-img
+        :src="'https://polskazachwyca.pl/wp-content/uploads/2017/11/moszna-shutterstock_45201820-e1511825136853.jpg'"
         aspect-ratio="1"
         class="grey lighten-2"
       >
--- web-app/src/components/Portfolio.vue
+++ web-app/src/components/Portfolio.vue
@@ -2,20 +2,20 @@
 <v-container class="my-15">
     <v-row justify="center">
         <v-col lg="12" class="text-center ">
-            <v-subheader class="text-h4 justify-center 	d-none d-sm-flex">Portfolio</v-subheader>
-            <v-subheader class="text-h4 justify-center 	d-flex d-sm-none">Portfolio</v-subheader>
-            <p>Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum</p>
+            <v-subheader class="text-h4 justify-center 	d-none d-sm-flex">Najpiekniejsze Polskie Miasta!</v-subheader>
+            <v-subheader class="text-h4 justify-center 	d-flex d-sm-none">Najpiekniejsze Polskie Miasta!</v-subheader>
+            <p>W tym miejscu możesz doświadczyć najpiekniejszych widoków Polski przed swoim ekranem!</p>
         </v-col>
         <v-col lg="12">
             <div class="justify-center mt-5 d-none d-sm-flex ">
-                <v-chip @click="activePortfolio = 'webapp'" :color="activePortfolio === 'webapp'? 'cyan':''" class="mx-5 ">Web App</v-chip>
-                <v-chip @click="activePortfolio = 'mobileapp'" :color="activePortfolio === 'mobileapp'? 'cyan':''" class="mx-5">Mobile App</v-chip>
-                <v-chip @click="activePortfolio = 'desktopapp'" :color="activePortfolio === 'desktopapp'? 'cyan':''" class="mx-5">Desktop App</v-chip>
+                <v-chip @click="activePortfolio = 'webapp'" :color="activePortfolio === 'webapp'? 'cyan':''" class="mx-5 ">Warszawa</v-chip>
+                <v-chip @click="activePortfolio = 'mobileapp'" :color="activePortfolio === 'mobileapp'? 'cyan':''" class="mx-5">Rzeszów</v-chip>
+                <v-chip @click="activePortfolio = 'desktopapp'" :color="activePortfolio === 'desktopapp'? 'cyan':''" class="mx-5">Kraków</v-chip>
             </div>
             <div class="justify-center mt-5 d-flex d-sm-none">
-                <v-chip @click="activePortfolio = 'webapp'" :color="activePortfolio === 'webapp'? 'cyan':''" class="mx-5 ">Web App</v-chip>
-                <v-chip @click="activePortfolio = 'mobileapp'" :color="activePortfolio === 'mobileapp'? 'cyan':''" class="mx-5">Mobile App</v-chip>
-                <v-chip @click="activePortfolio = 'desktopapp'" :color="activePortfolio === 'desktopapp'? 'cyan':''" class="mx-5">Desktop App</v-chip>
+                <v-chip @click="activePortfolio = 'webapp'" :color="activePortfolio === 'webapp'? 'cyan':''" class="mx-5 ">Warszawa</v-chip>
+                <v-chip @click="activePortfolio = 'mobileapp'" :color="activePortfolio === 'mobileapp'? 'cyan':''" class="mx-5">Rzeszów</v-chip>
+                <v-chip @click="activePortfolio = 'desktopapp'" :color="activePortfolio === 'desktopapp'? 'cyan':''" class="mx-5">Kraków</v-chip>
             </div>
         </v-col>
         <slot v-if="activePortfolio === 'webapp'">
@@ -72,30 +72,30 @@ export default {
             return{
                 activePortfolio:'webapp',
                 webApps:[
-                    {title:'Web App 1', image:'https://images.pexels.com/photos/1545743/pexels-photo-1545743.jpeg?cs=srgb&dl=pexels-yurii-hlei-1545743.jpg&fm=jpg'},
-                    {title:'Web App 2', image:'https://images.pexels.com/photos/1545743/pexels-photo-1545743.jpeg?cs=srgb&dl=pexels-yurii-hlei-1545743.jpg&fm=jpg'},
-                    {title:'Web App 3', image:'https://images.pexels.com/photos/1545743/pexels-photo-1545743.jpeg?cs=srgb&dl=pexels-yurii-hlei-1545743.jpg&fm=jpg'},
-                    {title:'Web App 4', image:'https://images.pexels.com/photos/1545743/pexels-photo-1545743.jpeg?cs=srgb&dl=pexels-yurii-hlei-1545743.jpg&fm=jpg'},
-                    {title:'Web App 5', image:'https://images.pexels.com/photos/1545743/pexels-photo-1545743.jpeg?cs=srgb&dl=pexels-yurii-hlei-1545743.jpg&fm=jpg'},
-                    {title:'Web App 6', image:'https://images.pexels.com/photos/1545743/pexels-photo-1545743.jpeg?cs=srgb&dl=pexels-yurii-hlei-1545743.jpg&fm=jpg'},
+                    {title:'Starówka', image:'https://warsawtour.pl/wp-content/uploads/2018/08/Zamkowy-Square_fot.-m.st_.-Warszawa_-PZ-Studio_Zbigniew-Panow.jpg'},
+                    {title:'Łazienki', image:'https://upload.wikimedia.org/wikipedia/commons/f/fb/Pa%C5%82ac_na_wodzie_w_%C5%81azienkach_Kr%C3%B3lewskich_%28cropped%29.jpg'},
+                    {title:'Pomnik', image:'https://d-art.ppstatic.pl/kadry/k/r/d1/88/5b30ffd58c575_o_full.jpg'},
+                    {title:'Złote Tarasy', image:'https://i.iplsc.com/-/000DK3NUOOF20T47-C466-F4.jpg'},
+                    {title:'Wiadukt', image:'https://zdm.waw.pl/wp-content/uploads/2021/07/20210630_wiadukt_Poniatowski_iluminacja-05100-scaled.jpg'},
+                    {title:'Pałac Kultury', image:'https://polskazachwyca.pl/wp-content/uploads/2017/08/shutterstock_190990370-1-e1516185680833.jpg'},
 
                     
                 ],
                 mobileApps:[
-                    {title:'Mobile App 1', image:'https://images.pexels.com/photos/1545743/pexels-photo-1545743.jpeg?cs=srgb&dl=pexels-yurii-hlei-1545743.jpg&fm=jpg'},
-                    {title:'Mobile App 2', image:'https://images.pexels.com/photos/1545743/pexels-photo-1545743.jpeg?cs=srgb&dl=pexels-yurii-hlei-1545743.jpg&fm=jpg'},
-                    {title:'Mobile App 3', image:'https://images.pexels.com/photos/1545743/pexels-photo-1545743.jpeg?cs=srgb&dl=pexels-yurii-hlei-1545743.jpg&fm=jpg'},
-                    {title:'Mobile App 4', image:'https://images.pexels.com/photos/1545743/pexels-photo-1545743.jpeg?cs=srgb&dl=pexels-yurii-hlei-1545743.jpg&fm=jpg'},
-                    {title:'Mobile App 5', image:'https://images.pexels.com/photos/1545743/pexels-photo-1545743.jpeg?cs=srgb&dl=pexels-yurii-hlei-1545743.jpg&fm=jpg'},
-                    {title:'Mobile App 6', image:'https://images.pexels.com/photos/1545743/pexels-photo-1545743.jpeg?cs=srgb&dl=pexels-yurii-hlei-1545743.jpg&fm=jpg'},
+                    {title:'Pomnik Czynu Rew.', image:'https://nadzory-archeologiczne.pl/wp-content/uploads/2014/03/Rzesz%C3%B3w-1024x493.jpg'},
+                    {title:'Kładka', image:'https://www.skanska.pl/globalassets/externalcontent2/project/okrga-kadka-dla-pieszych-w-rzeszowie/13f68a08-8715-4b28-a772-071396bcc9a4.1.jpg?height=524&width=932&scale=both&mode=crop&bgcolor='},
+                    {title:'Rynek', image:'https://erzeszow.pl/static/img/k01/WPM/rynek/min1200/r1.jpg'},
+                    {title:'Galeria Rzeszów', image:'https://pliki.propertynews.pl/i/00/63/02/006302_r0_940.jpg'},
+                    {title:'Pałac Lubomirskich', image:'https://erzeszow.pl/static/img/k01/atrakcje/min1200/lubomirscy_zamek.jpg'},
+                    {title:'Pałacyk Letni', image:'https://d-art.ppstatic.pl/kadry/k/r/25/b1/5e5e7034b327e_o_full.jpg'},
                 ],
                 desktopApps:[
-                    {title:'Desktop App 1', image:'https://images.pexels.com/photos/1545743/pexels-photo-1545743.jpeg?cs=srgb&dl=pexels-yurii-hlei-1545743.jpg&fm=jpg'},
-                    {title:'Desktop App 2', image:'https://images.pexels.com/photos/1545743/pexels-photo-1545743.jpeg?cs=srgb&dl=pexels-yurii-hlei-1545743.jpg&fm=jpg'},
-                    {title:'Desktop App 3', image:'https://images.pexels.com/photos/1545743/pexels-photo-1545743.jpeg?cs=srgb&dl=pexels-yurii-hlei-1545743.jpg&fm=jpg'},
-                    {title:'Desktop App 4', image:'https://images.pexels.com/photos/1545743/pexels-photo-1545743.jpeg?cs=srgb&dl=pexels-yurii-hlei-1545743.jpg&fm=jpg'},
-                    {title:'Desktop App 5', image:'https://images.pexels.com/photos/1545743/pexels-photo-1545743.jpeg?cs=srgb&dl=pexels-yurii-hlei-1545743.jpg&fm=jpg'},
-                    {title:'Desktop App 6', image:'https://images.pexels.com/photos/1545743/pexels-photo-1545743.jpeg?cs=srgb&dl=pexels-yurii-hlei-1545743.jpg&fm=jpg'},
+                    {title:'Starówka', image:'https://fajnepodroze.pl/wp-content/uploads/2019/04/stare-miasto-rynek-2_1.jpg'},
+                    {title:'Smok Wawelski', image:'https://i.iplsc.com/-/000EDCLNCAHI58PU-C411.jpg'},
+                    {title:'Kościół Mariacki', image:'https://polskazachwyca.pl/wp-content/uploads/2018/01/ko%C5%9Bci%C3%B3%C5%82-mariacki-krak%C3%B3w-shutterstock_667256869-e1516255860888.jpg'},
+                    {title:'Bulwary Krakowskie', image:'https://cdn.galleries.smcloud.net/t/galleries/gf-zbRn-rRbF-BDcS_zdjecie-dnia-664x442-nocrop.jpg'},
+                    {title:'Sukiennice', image:'https://dziendobry.tvn.pl/najnowsze/cdn-zdjecie-59jxf8-krakowskie-sukiennice-5707452/alternates/LANDSCAPE_1280'},
+                    {title:'Miasto nocą', image:'https://krainawycieczek.pl/wp-content/uploads/2017/12/Krak%C3%B3w-noc%C4%85.jpg'},
                 ]
             }
           }
